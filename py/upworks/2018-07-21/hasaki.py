254862ef9e726c4f6deb6c0dfe727c09ecfa9e08
hieutkt <hieunguyen31371@gmail.com>
2018-09-30 16:51:17 +0700

    Update hasaki.py

--- py/upworks/2018-07-21/hasaki.py
+++ py/upworks/2018-07-21/hasaki.py
@@ -8,16 +8,22 @@ import glob, os
 import re
 import schedule
 import zipfile
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.support.ui import Select
+
+
 
 SITE_NAME = "hasaki"
-BASE_URL = "https://www.hasaki.vn/"
+BASE_URL = "https://hasaki.vn"
 PROJECT_PATH = os.getcwd()
 PROJECT_PATH = PROJECT_PATH.replace("\\",'/')
 PATH_HTML = PROJECT_PATH + "/html/" + SITE_NAME + "/"
 PATH_CSV = PROJECT_PATH + "/csv/" + SITE_NAME + "/"
 CHROME_DRIVER_PATH = "bin/chromedriver"
-DATE = str(datetime.date.today())
-
 
 def write_csv(data):
     file_exists = os.path.isfile(PATH_CSV + SITE_NAME + "_" + DATE + ".csv")
@@ -26,8 +32,8 @@ def write_csv(data):
     with open(PATH_CSV + SITE_NAME + "_" + DATE + ".csv", 'a', newline='', encoding='utf-8-sig') as f:
         writer = csv.writer(f, delimiter=',')
         if not file_exists:
-            writer.writerow(('category', 'sub_category', 'sub_sub_category', 'id', 'title_Vietnamese', 'title_English', 'price', 'old_price', 'date'))
-        writer.writerow((data['category'],data['sub_category'],data['sub_sub_category'],data['id'], data['title_Vietnamese'], data['title_English'], data['price'], data['old_price'], data['date']))
+            writer.writerow(('category', 'vietnam_name', 'english_name', 'brand', 'price', 'old_price', 'date'))
+        writer.writerow((data['category'], data['vietnam_name'], data['english_name'], data['brand'], data['price'], data['old_price'], data['date']))
 
 def write_html(html, file_name):
     if not os.path.exists(PATH_HTML):
@@ -36,147 +42,146 @@ def write_html(html, file_name):
         f.write(html)
 
 def daily_task():
+    global DATE
+    DATE = str(datetime.date.today())
     chromeOptions = webdriver.ChromeOptions()
     prefs = {"profile.managed_default_content_settings.images":2}
     chromeOptions.add_argument("--headless")
     chromeOptions.add_experimental_option("prefs",prefs)
     browser = webdriver.Chrome(chrome_options=chromeOptions,executable_path=CHROME_DRIVER_PATH)
-    # browser = webdriver.Chrome()
+    # browser = webdriver.Chrome(chrome_options=chromeOptions)
     browser.set_window_position(400, 40)
     browser.set_window_size(1300, 1024)
-    browser.get("https://www.hasaki.vn/")
-    soup = BeautifulSoup(browser.page_source, 'lxml')
-    # /html/body/div/div[2]
-    waits = browser.find_element_by_xpath('/html/body/div/div[2]').text
-    while "Thank you" in waits:
-        time.sleep(1)
-        browser.refresh()
-        waits = browser.find_element_by_xpath('/html/body/div/div[2]').text
-    soup = BeautifulSoup(browser.page_source, 'lxml')
+    wait = ui.WebDriverWait(browser,60)
+    wait2 = ui.WebDriverWait(browser,30)
     urls = []
-    main_category_list = soup.find('div', class_='col_menu_cap1').find_all('a', class_='parent_menu')
-    for item in main_category_list:
-        href = "https://hasaki.vn/" + item.get('href')
-        if "?" not in href:
-            urls.append(href)
-    main_category_list = soup.find('div', class_='col_menu_cap1').find_all('div', class_='col_hover_submenu')
-    for main_item in main_category_list:
-        category_list = main_item.find_all('a')
-        for item in category_list:
-            href = "https://hasaki.vn/" + item.get('href')
-            if "?" not in href:
-                urls.append(href)
-    j=0
+    urls = ['https://hasaki.vn/suc-khoe-lam-dep.html', 'https://hasaki.vn/suc-khoe-lam-dep/trang-diem.html', 'https://hasaki.vn/suc-khoe-lam-dep/cham-soc-da-mat.html', 'https://hasaki.vn/suc-khoe-lam-dep/cham-soc-co-the.html', 'https://hasaki.vn/suc-khoe-lam-dep/cham-soc-toc.html', 'https://hasaki.vn/me-va-be.html', 'https://hasaki.vn/me-va-be/be-ve-sinh.html', 'https://hasaki.vn/me-va-be/be-an.html', 'https://hasaki.vn/spa.html', 'https://hasaki.vn/hasaki-spa/giam-beo.html', 'https://hasaki.vn/hasaki-spa/triet-long-vinh-vien.html']
+    titles = []
+    titles = ['Sức Khỏe Làm Đẹp', 'Trang Điểm', 'Chăm Sóc Da Mặt', 'Chăm Sóc Cơ Thể', 'Chăm Sóc Tóc', 'Ba Mẹ Và Bé - Đồ chơi', 'Bé Vệ Sinh', 'Bé Ăn', 'Hasaki Clinic & Spa', 'Giảm Béo', 'Triệt Lông']
+    browser.get(BASE_URL)
+    soup = BeautifulSoup(browser.page_source, 'lxml')
     write_html(browser.page_source, "All_cat_")
+    menu_list = soup.find('div', id='sub_menu_web').find_all('a', class_='parent_menu')
+    for item in menu_list:
+        href = BASE_URL + item.get('href')
+        title = item.text.strip()
+        if href not in urls:
+            urls.append(href)
+            titles.append(title)
+        # browser.get(BASE_URL+item.get('href'))
+        # soup = BeautifulSoup(browser.page_source, 'lxml')
+        # try:
+        #     menu_list2 = soup.find('div', class_='content_fillter').find_all('a', class_='item_fillter')
+        # except:
+        #     continue
+        # for item2 in menu_list2:
+        #     href = item2.get('href')
+        #     title = item2.text.strip()
+        #     if href not in urls:
+        #         urls.append(href)
+        #         titles.append(title)
+    # print(len(titles))
+    # print(titles)
+    j=0
     while j < len(urls):
+        
         browser.get(urls[j])
-        waits = browser.find_element_by_xpath('/html/body/div/div[2]').text
-        while "Thank you" in waits:
-            time.sleep(1)
-            browser.refresh()
-            waits = browser.find_element_by_xpath('/html/body/div/div[2]').text
-        soup = BeautifulSoup(browser.page_source, 'lxml')
-        # if browser.find_element_by_xpath('//*[@id="lb_thongbao_canhtranh"]').is_displayed():
-        #     element = browser.find_element_by_xpath('//*[@id="lb_thongbao_canhtranh"]/div[1]/button')
-        #     browser.execute_script("arguments[0].click();", element)
-        product_is = soup.find('div', class_='empty')
-        if product_is != None:
-            j+=1
-            continue
-        category_titles = soup.find('nav', id='breadcrumb').find_all('li')
-        if len(category_titles) == 2:
-            category = category_titles[1].find('a').text.strip()
-            sub_category = None
-            sub_sub_category = None
-        if len(category_titles) == 3:
-            category = category_titles[1].find('a').text.strip()
-            sub_category = category_titles[2].find('a').text.strip()
-            sub_sub_category = None
-        if len(category_titles) == 4:
-            category = category_titles[1].find('a').text.strip()
-            sub_category = category_titles[2].find('a').text.strip()
-            sub_sub_category = category_titles[3].find('a').text.strip()
+
+        category = titles[j]
+
 
         # print(page_count)
+
         i=0
-        is_next_elem = True
-        while is_next_elem:
-            soup = BeautifulSoup(browser.page_source, 'lxml')
-            if soup.find('ul', class_='pagination') != None:
-                page_count = soup.find('ul', class_='pagination').find_all('li')
+        pagination = True
+        while pagination:
             if i != 0:
-                local_url = browser.find_element_by_xpath('//*[@id="col_right"]/nav/ul/li[' + str(len(page_count)) + ']/a')
-                # //*[@id="col_right"]/nav/ul/li[6]/a
-                local_url = local_url.get_attribute('href')
-                # print(local_url)
-                browser.get(str(local_url))
-                soup = BeautifulSoup(browser.page_source, 'lxml')
-                waits = browser.find_element_by_xpath('/html/body/div/div[2]').text
-                while "Thank you" in waits:
-                    time.sleep(1)
-                    browser.refresh()
-                    waits = browser.find_element_by_xpath('/html/body/div/div[2]').text
-                # wait.until(lambda browser: browser.find_element_by_xpath('//*[@id="col_right"]/nav'))
-                soup = BeautifulSoup(browser.page_source, 'lxml')
-                list = soup.find_all('div', class_='product_item')
+                try:
+                    wait.until(lambda browser: browser.find_element_by_css_selector('#col_right > nav > ul'))
+                    elements = browser.find_elements_by_css_selector('#col_right > nav > ul > li')
+                    c=0
+                    while c < len(elements):
+                        class_name = elements[c].get_attribute("class")
+                        if "active" in class_name:
+                            if len(elements)-1 >= c+1:
+                                href_glob = elements[c+1].find_element_by_css_selector('a').get_attribute("href")
+                                browser.get(href_glob)
+                                c+=1
+                                break
+                            else:
+                                pagination = False
+                                c+=1
+                                break
+                        c+=1
+                except NoSuchElementException:
+                    pagination = False
+                except TimeoutException:
+                    pagination = False
+                except:
+                    pagination = False
+                try:
+                    soup = BeautifulSoup(browser.page_source, 'lxml')
+                    list = soup.find('div', class_='list_product').find_all('div', class_='product_item')
+                except:
+                    pagination = False
             if i == 0:
-                soup = BeautifulSoup(browser.page_source, 'lxml')
-                list = soup.find_all('div', class_='product_item')
+                try:
+                    soup = BeautifulSoup(browser.page_source, 'lxml')
+                    list = soup.find('div', class_='list_product').find_all('div', class_='product_item')
+                except:
+                    pagination = False
+            if pagination == False:
+                break
             # print(len(list))
             # print(i+1)
             for item in list:
-                item_id = item.find('a').get('href').strip()
-                # item_id = item_id.split('id=')[1]
-                # Vietnamese
-                # English
-                if item.find('div', class_='vietnam_name') != None:
-                    title_Vietnamese = item.find('div', class_='vietnam_name').text.strip()
-                else:
-                    title_Vietnamese = None
-                if item.find('div', class_='english_name') != None:
-                    title_English = item.find('div', class_='english_name').text.strip()
-                else:
-                    title_English = None
-                # print("Title: " + title)
-                if item.find('span', class_='giamoi') != None:
+                # ---name,
+                # ---price,
+                # ---brand
+                # ---old_price (previous price if exists),
+                # ---category (name of category),
+                # ---current date
+
+                try:
+                    brand = item.find('div', class_='product_shopping').text.strip()
+                except:
+                    brand = None
+
+                try:
+                    vietnam_name = item.find('div', class_='vietnam_name').text.strip()
+                except:
+                    vietnam_name = None
+
+                try:
+                    english_name = item.find('div', class_='english_name').text.strip()
+                except:
+                    english_name = None
+
+                try:
                     price = item.find('span', class_='giamoi').text.strip()
-                    price = price.split('₫')[0]
-                    price = price.strip()
-                else:
+                    # price = price.split('đ')[0]
+                    # price = price.strip()
+                except:
                     price = None
-                # print("Price: " + str(price))
-                if item.find('span', class_='giacu') != None:
+
+                try:
                     old_price = item.find('span', class_='giacu').text.strip()
-                    # old_price = old_price.split('₫')[0]
-                    old_price = old_price.strip()
-                else:
+                    # old_price = old_price.split('đ')[0]
+                    # old_price = old_price.strip()
+                except:
                     old_price = None
                 
-                date = DATE
 
                 data = {'category': category,
-                        'sub_category': sub_category,
-                        'sub_sub_category': sub_sub_category,
-                        'id': item_id,
-                        'title_Vietnamese': title_Vietnamese,
-                        'title_English': title_English,
+                        'vietnam_name': vietnam_name,
+                        'english_name': english_name,
+                        'brand': brand,
                         'price': price,
                         'old_price': old_price,
-                        'date': date}
+                        'date': DATE}
                 write_csv(data)
             file_name = str(j+1) + "_" + str(i+1) + "_"
             write_html(browser.page_source, file_name)
-            soup = BeautifulSoup(browser.page_source, 'lxml')
-            if soup.find('ul', class_='pagination') != None:
-                page_count = soup.find('ul', class_='pagination').find_all('li')
-            for item in page_count:
-                if item.find('a') != None:
-                    next_elem = item.find('a').get('aria-label')
-                    if next_elem == "Next":
-                        is_next_elem = True
-                        break
-                    else:
-                        is_next_elem = False
             i+=1
         j+=1
     browser.quit()
@@ -195,7 +200,6 @@ def compress_data():
         z.write(file)
         os.remove(file)
 
-
 if "test" in sys.argv:
     daily_task()
 else:
@@ -203,6 +207,3 @@ else:
     while True:
         schedule.run_pending()
         time.sleep(1)
-
-# if __name__ == '__main__':
-#     daily_task()
