faff3672f39fc9c95152133dd1e5f3905f134b52
hieutkt <hieunguyen31371@gmail.com>
2020-07-21 18:45:37 +0700

    Fix bachhoaxanh.com

--- py/prices_bachhoaxanh.py
+++ py/prices_bachhoaxanh.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Price crawler for bachhoaxanh.com """
+# TODO "View more" button: "li.nextPaging > a"
+
+# builtin module
 import sys
-import os, glob
+import os
+import glob
 from zipfile import ZipFile
 import time
 import datetime
@@ -7,10 +15,15 @@ import schedule
 import re
 import csv
 import random
-import coloredlogs, logging
+# loggings
+import coloredlogs
+import logging
 import logging.handlers as handlers
-from urllib.request import urlopen
+# scrapings
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import signal
 
 
 # Parameters
@@ -21,8 +34,15 @@ PATH_HTML = PROJECT_PATH + "/html/" + SITE_NAME + "/"
 PATH_CSV = PROJECT_PATH + "/csv/" + SITE_NAME + "/"
 PATH_LOG = PROJECT_PATH + "/log/"
 DATE = str(datetime.date.today())
-
 OBSERVATION = 0
+
+
+# Selenium options
+OPTIONS = webdriver.ChromeOptions()
+OPTIONS.add_argument('--headless')
+OPTIONS.add_argument('--disable-gpu')
+OPTIONS.add_argument("--width=2560")
+OPTIONS.add_argument("--height=1440")
 
 # Setting up logging
 if not os.path.exists(PATH_LOG):
@@ -34,8 +54,9 @@ log_format = logging.Formatter(
 )
 log_writer = logging.FileHandler(PATH_LOG + SITE_NAME + '.log')
 log_stout = logging.StreamHandler()
-log_error = handlers.TimedRotatingFileHandler(PATH_LOG + 'aggregated_error/errors.log',
-    when = 'midnight', interval=1)
+log_error = handlers.TimedRotatingFileHandler(
+    PATH_LOG + 'aggregated_error/errors.log',
+    when='midnight', interval=1)
 log_error.suffix = '%Y-%m-%d_' + SITE_NAME
 
 log_writer.setFormatter(log_format)
@@ -67,12 +88,17 @@ def main():
 def daily_task():
     """Main workhorse function. Support functions defined below"""
     global CATEGORIES_PAGES
+    global BROWSER
     global DATE
     global OBSERVATION
     logging.info('Scraper started')
     # Refresh date
     DATE = str(datetime.date.today())
     OBSERVATION = 0
+    # Initiate headless web browser
+    logging.debug('Initialize browser')
+    BROWSER = webdriver.Chrome(executable_path=PROJECT_PATH + "/bin/chromedriver",
+                               options=OPTIONS)
     # Download topsite and get categories directories
     base_file_name = "All_cat_" + DATE + ".html"
     fetch_html(BASE_URL, base_file_name, PATH_HTML, attempts_limit=1000)
@@ -85,7 +111,10 @@ def daily_task():
         download = fetch_html(cat['directlink'], cat_file, PATH_HTML)
         if download:
             scrap_data(cat)
-            # find_next_page(cat)
+    # Close browser
+    BROWSER.close()
+    BROWSER.service.process.send_signal(signal.SIGTERM)
+    BROWSER.quit()
 
 
 def fetch_html(url, file_name, path, attempts_limit=5):
@@ -96,11 +125,11 @@ def fetch_html(url, file_name, path, attempts_limit=5):
         attempts = 0
         while attempts < attempts_limit:
             try:
-                con = urlopen(url, timeout=5)
-                html_content = con.read()
-                with open(path + file_name, "wb") as f:
+                BROWSER.get(url)
+                element = BROWSER.find_element_by_xpath("/html")
+                html_content = element.get_attribute("innerHTML")
+                with open(path + file_name, "w") as f:
                     f.write(html_content)
-                    con.close
                 logging.debug("Downloaded " + file_name)
                 return(True)
             except Exception as e:
@@ -116,7 +145,7 @@ def fetch_html(url, file_name, path, attempts_limit=5):
 
 
 def get_category_list(top_html):
-    """Get list of relative categories directories from the top page"""
+    """Get list o relative categories directories from the top page"""
     page_list = []
     toppage_soup = BeautifulSoup(top_html, "lxml")
     categories = toppage_soup.find('ul', {'class': 'colmenu-ul'})
@@ -125,11 +154,11 @@ def get_category_list(top_html):
     categories_tag = [item for sublist in categories_tag for item in sublist]
     for cat in categories_tag:
         page = {}
-        link = re.sub(".+bachhoaxanh\.com/", "", cat['href'])
+        link = re.sub(".+bachhoaxanh\\.com/", "", cat['href'])
         page['relativelink'] = link
         page['directlink'] = BASE_URL + link
         page['name'] = re.sub("/|\\?.=", "_", link)
-        page['label'] = cat.text
+        page['label'] = cat.text.strip()
         page_list.append(page)
     # Remove duplicates
     page_list = [dict(t) for t in set(tuple(i.items()) for i in page_list)]
@@ -139,44 +168,39 @@ def get_category_list(top_html):
 def scrap_data(cat):
     """Get item data from a category page and write to csv"""
     global OBSERVATION
-    cat_file = open(PATH_HTML + "cat_" + cat['name'] + "_" +
-                    DATE + ".html").read()
-    cat_soup = BeautifulSoup(cat_file, "lxml")
-    cat_div = cat_soup.find("div", {"id": "boxproduct"})
+    # Click on "View more" until it disapears
+    # is_next_button = True
+    # while is_next_button:
+    #     try:
+    #         viewmore_button = BROWSER.find_element_by_css_selector(
+    #             "li.nextPaging > a.viewmore")
+    #         # BROWSER.execute_script("arguments[0].scrollIntoView();",
+    #         #                        viewmore_button)
+    #         time.sleep(5)
+    #         viewmore_button.click()
+    #     except NoSuchElementException:
+    #         is_next_button = False
+    # Get html source
+    soup = BeautifulSoup(BROWSER.page_source, 'lxml')
+    cat_div = soup.find("div", {"class": "boxproduct"})
     cat_div = cat_div.findAll("li", {"class": "product"}) if cat_div else None
     if cat_div is None:
         cat_div = []
     for item in cat_div:
         row = {}
-        good_name = item.find('h3')
+        good_name = item.find("div", {"class": "product-name"})
         row['good_name'] = good_name.text.strip() if good_name else None
         price = item.find('div', {"class": "price"})
-        row['price'] = price.span.text.strip() if price else None
-        old_price = price.find('strong') if price else None
-        row['old_price'] = old_price.contents[0] if old_price else None
+        row['price'] = price.strong.text.strip() if price else None
+        old_price = price.find('span') if price else None
+        row['old_price'] = old_price.text.strip() if old_price else None
         id1 = item.get('data-product')
         row['id'] = id1
         row['category'] = cat['name']
         row['category_label'] = cat['label']
         row['date'] = DATE
-        OBSERVATION += 1        
+        OBSERVATION += 1
         write_data(row)
-
-
-def find_next_page(cat):
-    """Find the next page button, return page data"""
-    cat_file = open(PATH_HTML + "cat_" + cat['name'] + "_" +
-                    DATE + ".html").read()
-    cat_soup = BeautifulSoup(cat_file, "lxml")
-    next_button = cat_soup.find("a", {"class": "btn", "rel": "next"})
-    if next_button:
-        link = re.sub(".+adayroi\.com", "", next_button['href'])
-        if link not in [i['relativelink'] for i in CATEGORIES_PAGES]:
-            next_page = cat.copy()
-            next_page['relativelink'] = link
-            next_page['directlink'] = BASE_URL + link
-            next_page['name'] = re.sub("/|\\?.=", "_", link)
-            CATEGORIES_PAGES.append(next_page)
 
 
 def write_data(item_data):
@@ -199,7 +223,7 @@ def compress_csv():
         os.makedirs(PATH_CSV)
     os.chdir(PATH_CSV)
     try:
-        zip_csv = ZipFile(SITE_NAME + '_' + DATE + '_csv.zip', 'a') #
+        zip_csv = ZipFile(SITE_NAME + '_' + DATE + '_csv.zip', 'a')
         for file in glob.glob("*" + DATE + "*" + "csv"):
             zip_csv.write(file)
             os.remove(file)
@@ -216,7 +240,7 @@ def compress_html():
         os.makedirs(PATH_HTML)
     os.chdir(PATH_HTML)
     try:
-        zip_csv = ZipFile(SITE_NAME + '_' + DATE + '_html.zip', 'a') #
+        zip_csv = ZipFile(SITE_NAME + '_' + DATE + '_html.zip', 'a')
         for file in glob.glob("*" + DATE + "*" + "html"):
             zip_csv.write(file)
             os.remove(file)
@@ -233,7 +257,7 @@ if "test" in sys.argv:
 else:
     if "run" in sys.argv:
         main()
-    start_time = '01:' + str(random.randint(0,59)).zfill(2)
+    start_time = '01:' + str(random.randint(0, 59)).zfill(2)
     schedule.every().day.at(start_time).do(main)
     while True:
         schedule.run_pending()
