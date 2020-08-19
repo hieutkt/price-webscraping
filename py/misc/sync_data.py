dcfc841f07409512e61649320ecd69ee0b0cbc22
hieutkt <hieunguyen31371@gmail.com>
2020-08-19 18:35:01 +0700

    Add: `sync_data.py` in py/misc/

---
+++ py/misc/sync_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import schedule
+import sys
+import os
+import datetime
+import time
+
+
+def sync_data(data_dir='csv/', backup_dir='backup_csv/'):
+    sync_command = 'rsync -av ' + data_dir + ' ' + backup_dir
+    os.system(sync_command)
+    print('Finished synching data at: ' + str(datetime.datetime.now()))
+
+
+if "test" in sys.argv:
+    sync_data()
+else:
+    schedule.every().day.at("20:00").do(sync_data)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
