27059bf6f805b2d7262424107b15280acc433fa1
QuangPhan1303 <62643228+QuangPhan1303@users.noreply.github.com>
2020-08-20 17:46:29 +0700

    Fix: recoding as the requirement
    + compress.py
    - change "import loggings_setup" to "from misc.loggings_setup import setup_loggings"
    
    + io.py
    - remove the "class Output"
    - change the "field_name" function

--- py/misc/io.py
+++ py/misc/io.py
@@ -4,8 +4,7 @@ import time
 import csv
 
 def write_data(item_data):
-
-    fieldnames = ['good_name', 'price', 'old_price', 'id', 'category', 'category_label', 'date']
+    fieldnames = item_data.keys()
     file_exist = os.path.isfile(csv_path + site_name + "_" + date + ".csv")
 if not os.path.exists(csv_path):
     os.makedirs(csv_path)
