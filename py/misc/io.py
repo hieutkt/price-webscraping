f0fd1458cfededc7654ac54818de807d8791b74b
QuangPhan1303 <62643228+QuangPhan1303@users.noreply.github.com>
2020-08-20 16:45:11 +0700

    Revert "Compress"
    This reverts commit 3851df1f07669190447a27fb7a220f1b606e1076.

--- py/misc/io.py
+++ py/misc/io.py
@@ -3,25 +3,14 @@ import datetime
 import time
 import csv
 
-class Output:
-    def __init__(self, good_name, price, old_price, id, category, category_label, date = str(datetime.date.today())):
-       self.good_name = good_name
-       self.price = price
-       self.old_price = old_price
-       self.id = id
-       self.category = category
-       self.category_label = category_label
-       self.date = date
+def write_data(item_data):
 
-    def write_data(item_data):
-        fieldnames = [self.good_name, self.price, self.old_price, self.id, self.category, self.category_label, self.date]
-        file_exist = os.path.isfile(csv_path + site_name + "_" + date + ".csv")
-    if not os.path.exists(csv_path):
-        os.makedirs(csv_path)
-    with open(csv_path + site_name + "_" + date + ".csv", "a") as f:
-        writer = csv.DictWriter(f, fieldnames)
-        if not file_exists: 
-            writer.writehearder()
-        writer.writerow(item_data)
-
-    coloredlogs.install()
+    fieldnames = ['good_name', 'price', 'old_price', 'id', 'category', 'category_label', 'date']
+    file_exist = os.path.isfile(csv_path + site_name + "_" + date + ".csv")
+if not os.path.exists(csv_path):
+    os.makedirs(csv_path)
+with open(csv_path + site_name + "_" + date + ".csv", "a") as f:
+    writer = csv.DictWriter(f, fieldnames)
+    if not file_exists: 
+        writer.writehearder()
+    writer.writerow(item_data)
